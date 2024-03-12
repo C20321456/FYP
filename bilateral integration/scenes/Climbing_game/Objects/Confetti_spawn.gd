52db73a8a3433a13acbf0a91eaac8d6fb330e8f6
Michael Cooper <C20321456@mytudublin.ie>
2024-03-12 13:31:50 +0000

    made confetti colours, onto making clickable text

--- bilateral integration/scenes/Climbing_game/Objects/Confetti_spawn.gd
+++ bilateral integration/scenes/Climbing_game/Objects/Confetti_spawn.gd
@@ -1,16 +1,24 @@
 extends MeshInstance3D
 
-var colors = [Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)] # List of colors to cycle through
+var colors =[
+				Color(1, 0, 0), # Red
+				Color(0, 1, 0),  # Green
+				Color(0, 0, 1),  # Blue
+				Color(5, 0, 5),  # Pink
+				Color(5, 5, 0),  # Yellow
+				Color(0, 5, 4),  # Cyan
+				Color(79, 0, 87) # Purple
+			]
+				# List of colors to cycle through
 var current_color_index = 0
 var timer_node # Variable to hold reference to the Timer node
 
 func _ready():
 	# Attempt to find the Timer node
-	timer_node = get_node("Timer")
-	
+	timer_node = get_node("../Timer")
 	# If Timer node is found, connect its timeout signal
 	if timer_node:
-		timer_node.connect("timeout", self, "_on_timer_timeout") # Adjusted method name
+		timer_node.timeout.connect(_on_timer_timeout) # Adjusted method name
 		# Start the Timer
 		timer_node.start()
 	else:
